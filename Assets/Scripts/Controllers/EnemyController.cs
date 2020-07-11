725e849a2c880f792bd692a30bd8099a8bc6ce57
Ben Specht <s3604514@student.rmit.edu.au>
2020-07-11 14:43:46 +1000

    Basic game loop (enemies dying and moving, cannon shooting and aiming)

---
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using Generics;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        public GenericEnemy enemyStats;
+
+        private enum Phase
+        {
+            Move,
+            Shoot
+        }
+
+        private Phase _currentPhase = Phase.Move;
+
+        [FormerlySerializedAs("Health")] public float health;
+        // Start is called before the first frame update
+        void Start()
+        {
+            SpriteRenderer rend = GetComponent<SpriteRenderer>();
+            rend.sprite = enemyStats.sprite;
+        
+            health = enemyStats.enemyHealth;
+        }
+        
+        // While X<1.5, Move right. Stop at X=1.5 and Shoot at Bus
+        
+        public void DamageEnemy(int damage)
+        {
+            health -= damage;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+            if (_currentPhase == Phase.Move)
+            {
+                Vector2 vec2pos = transform.position;
+                Vector2 targetPos = new Vector2(1.5f, vec2pos.y);
+                float step = enemyStats.enemySpeed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(vec2pos, targetPos, step);
+
+                if (vec2pos == targetPos)
+                {
+                    _currentPhase = Phase.Shoot;
+                }
+            } else if (_currentPhase == Phase.Shoot)
+            {
+                // Shooting code here
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Controllers
+{
+    public class GameManager : MonoBehaviour
+    {
+        public GameObject projPrefab;
+        public GameObject enemyAPrefab;
+
+        private Camera cam;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            cam = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var newProj = Instantiate(projPrefab, GameObject.Find("BulletStartPos").transform.position, Quaternion.identity);
+                projPrefab.GetComponent<ProjectileController>().target = cam.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                Vector3 spawnPos = new Vector3(-10, Random.Range(4.0f, -4.0f));
+                Instantiate(enemyAPrefab, spawnPos, Quaternion.identity);
+            }
+        }
+    }
+}
