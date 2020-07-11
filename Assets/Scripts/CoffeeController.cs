8cc491c31efec4351699c93978203dfeb5a21e67
Jack Belcher <jbelcher@panviva.com>
2020-07-11 11:34:22 +1000

    Added some scripts for coffee game

---
+++ Assets/Scripts/CoffeeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeController : MonoBehaviour
+{
+    public CoffeeCup currentCup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewCup()
+    {
+
+    }
+
+    public void grindCoffee()
+    {
+
+    }
+
+    public void steamMilk()
+    {
+
+    }
+
+    public void pourCoffee()
+    {
+
+    }
+
+    public void sendCup()
+    {
+
+    }
+}
---
+++ Assets/Scripts/CoffeeCup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeCup : MonoBehaviour
+{
+    public float grindQuality;
+    public float milkQuality;
+    public float pourQuality;
+    public float totalQuality;
+}
