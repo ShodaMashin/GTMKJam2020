475df5a7af550b85ded4a9fe883739f4b9982daa
Ben Specht <s3604514@student.rmit.edu.au>
2020-07-12 14:13:30 +1000

    Bus cannon rotation

--- Assets/Scripts/Controllers/BusController.cs
+++ Assets/Scripts/Controllers/BusController.cs
@@ -1,23 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BusController : MonoBehaviour
+namespace Controllers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BusController : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            var busHolder = GameObject.Find("BusCannon");
+            if (Camera.main == null) return;
+            var mouse = Input.mousePosition;
+            var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+            var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+            var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+            busHolder.transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+        }
 
-    public void DamageBus(int damage)
-    {
-        Storage.BusHealth -= damage;
+        public void DamageBus(int damage)
+        {
+            Storage.BusHealth -= damage;
+        }
     }
 }
